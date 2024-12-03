275c49880b82c100fe257761281aea7f509b3bf4
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-12-03 20:39:44 -0300

    Funcoes de calculo

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -3,7 +3,20 @@ import 'dart:io';
 
 void soma(int num1, int num2){
         print(num1! + num2!);
-    }
+}
+
+void sub(int num1, int num2){
+    print(num1! - num2!);
+}
+
+void mult(int num1, int num2){
+    print(num1! * num2!);
+}
+
+void divis(int num1, int num2){
+    print(num1! / num2!);
+}
+
 
 void main() {
     int num1 = int.parse(stdin.readLineSync()!);
