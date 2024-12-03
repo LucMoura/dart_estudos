dc23f0445ec6c082a7b985866c3a70fd8efb99cb
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-12-03 20:57:39 -0300

    MAIS FUNCOES

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -2,22 +2,22 @@ import 'dart:io';
 
 
 void soma(int num1, int num2){
-        print(num1! + num2!);
+        print(num1 + num2);
 }
 
 void sub(int num1, int num2){
-    print(num1! - num2!);
+    print(num1 - num2);
 }
 
 void mult(int num1, int num2){
-    print(num1! * num2!);
+    print(num1 * num2);
 }
 
 void divis(int num1, int num2){
-    print(num1! / num2!);
+    print(num1 / num2);
 }
 
-void inicio(int num1, int num2){
+void select(int num1, int num2){
     print("Selecione qual operação você deseja\n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão");
 
     var oper = stdin.readLineSync();
@@ -40,12 +40,15 @@ void inicio(int num1, int num2){
             break;
     }
 }
-
-
-void main() {
+void inicio(){
     print("Digite os números que deseja");
     int num1 = int.parse(stdin.readLineSync()!);
     int num2 = int.parse(stdin.readLineSync()!);
-    inicio(num1, num2);
+    select(num1, num2);
+}
+
+
+void main() {
+    inicio();
 
 }
